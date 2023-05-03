1fd1710644b8c62c1e3f70a4fe6f28f807d789e8
Luana Grazielly Soares da Paz <123707045+Luanagrazi@users.noreply.github.com>
2023-05-03 14:04:05 -0300

    Add files via upload

---
+++ html/questão2.java
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <script>
+       let n = 240;
+let c = parseInt(prompt("Digite o capital mensal meu nobre!!!"));
+let i = parseInt(prompt("Digite agora os juros meu nobre!!!"));
+
+let M = c * ((1 + (i/100))**n).toFixed(2);
+
+
+
+console.log(M)
+document.write(M)
+
+     <script type = "text/javascript">
+    </script>
+ 
+</head>
+<body>
+    
+</body>
+
+</html>
